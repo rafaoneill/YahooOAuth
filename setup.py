a41b1221605e24dc95f8c6ed760aaf0a4447f993
rafaoneill <rafael.oneill@gmail.com>
2019-09-28 14:01:28 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='YahooOAuth',
+    version=1.0,
+    description='Python module to consume Yahoo Fantasy API',
+    author='Rafael E. ONeill',
+    author_email='rafael.oneill@gmail.com',
+    packages=['yahoo-oauth'])

102a400fc8d4caad1739a1396d65c9ef54064734
rafaoneill <rafael.oneill@gmail.com>
2019-10-02 17:37:59 -0400

    Added collections to user resource

--- yahoooauth/resource/game.py
+++ yahoooauth/resource/game.py
@@ -1,4 +1,5 @@
 # https://developer.yahoo.com/fantasysports/guide/game-resource.html
+# https://developer.yahoo.com/fantasysports/guide/games-collection.html
 
 # Fetch game key, code, name, url, type and season. 
 # Params: {game_key}
@@ -27,3 +28,14 @@ YFANTASY_GAME_POSITION_TYPES='/fantasy/v2/game/{0}/position_types'
 # Fetch detailed description of all roster positions for the game.
 # Params: {game_key}
 YFANTASY_GAME_ROSTER_POSITIONS='/fantasy/v2/game/{0}/roster_positions'
+
+# Fetch specific games {game_key1} and {game_key2}
+# Params: {game_key1,game_key2,...}
+YFANTASY_SPECIFIC_GAMES='/fantasy/v2/games;game_keys={0}'
+
+# Fetch all games for the logged in user
+YFANTASY_GAMES_FOR_LOGGED_USER='/fantasy/v2/users;user_login=1/games'
+
+# Fetch specific games {game_key1} and {game_key2} that the logged in user owns teams in.
+# Params: {game_key1,game_key2,...}
+YFANTASY_SPECIFIC_GAMES_FOR_LOGGED_USER='/fantasy/v2/users;use_login=1/games;game_keys={0}'
